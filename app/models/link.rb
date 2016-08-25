e64c86ffd025c8f5c7c822fcbcfeb93b5cfd798d
Dennis Liaw <dennis.liaw@gmail.com>
2016-08-24 22:39:12 -0400

    adds ability to change conversion number base

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,15 +1,16 @@
 class Link < ApplicationRecord
 
   @@CODEX = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").zip((0..61)).to_h
+  @@BASE = 36
 
   def encode_url
     id = self.id
     digits = []
 
     while id > 0
-      remainder = id % 62
+      remainder = id % @@BASE
       digits << remainder
-      id /= 62
+      id /= @@BASE
     end
 
     digits.reverse.map { |num| @@CODEX.key num }.join("")
@@ -18,7 +19,7 @@ class Link < ApplicationRecord
   def self.decode_url(encoded_str)
     letters = encoded_str.split("").reverse
     exponents = letters.map { |letter| @@CODEX[letter] }
-    exponents.map.with_index { |value, id| value * (62**id) }.reduce(:+)
+    exponents.map.with_index { |value, id| value * (@@BASE**id) }.reduce(:+)
   end
 
   def duplicate?
