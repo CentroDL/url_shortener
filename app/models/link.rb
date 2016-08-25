65225a1cd1ac14251ed1cf6cb2e16f10001199e9
Dennis Liaw <dennis.liaw@gmail.com>
2016-08-24 22:52:22 -0400

    moves encode/decode code to the controller since the model doesn't persist any of the transformations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,29 +1,3 @@
 class Link < ApplicationRecord
 
-  @@CODEX = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").zip((0..61)).to_h
-  # @@BASE is 62 but can be trimmed to 36 if needed
-  @@BASE = @@CODEX.length
-
-  def encode_url
-    id = self.id
-    digits = []
-
-    while id > 0
-      remainder = id % @@BASE
-      digits << remainder
-      id /= @@BASE
-    end
-
-    digits.reverse.map { |num| @@CODEX.key num }.join("")
-  end
-
-  def self.decode_url(encoded_str)
-    letters = encoded_str.split("").reverse
-    exponents = letters.map { |letter| @@CODEX[letter] }
-    exponents.map.with_index { |value, id| value * (@@BASE**id) }.reduce(:+)
-  end
-
-  def duplicate?
-  end
-
 end
