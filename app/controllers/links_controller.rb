87e502397b75595b54ffb09f65f9428aa3f17e1a
Dennis Liaw <dennis.liaw@gmail.com>
2016-08-24 23:32:42 -0400

    adds stubs to links controller, trims some methods out

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,59 +1,47 @@
 class LinksController < ApplicationController
 
-  before_action: :duplicate?, only: [:create]
-
   # create
   def new
   end
 
   def create
-  end
-
-  # delete
-  def delete
-
-  end
-
-  # update
-  def edit
+    # check if already exists, return existing if need be
+    # create and redirect to index with message
+    # duplicates = Link.where
   end
 
   def update
-
-  end
-
-  # read
-  def index
-
-  end
-
-  def show
   end
 
   def top100
   end
 
-    @@CODEX = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").zip((0..61)).to_h
-  # @@BASE is 62 but can be trimmed to 36 if needed
-  @@BASE = @@CODEX.length
+  private
 
-  def encode_url
-    id = self.id
+  def link_params
+    params.require(:link).permit(:target)
+  end
+
+  CODEX = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").zip((0..61)).to_h
+  # BASE is 62 but can be trimmed to 36 if needed
+  BASE = CODEX.length
+
+  def encoded_url(id)
     digits = []
 
     while id > 0
       remainder = id % @@BASE
       digits << remainder
-      id /= @@BASE
+      id /= BASE
     end
 
-    digits.reverse.map { |num| @@CODEX.key num }.join("")
+    digits.reverse.map { |num| CODEX.key num }.join("")
   end
 
-  def self.decode_url(encoded_str)
+  def decoded_id(encoded_str)
     letters = encoded_str.split("").reverse
-    exponents = letters.map { |letter| @@CODEX[letter] }
-    exponents.map.with_index { |value, id| value * (@@BASE**id) }.reduce(:+)
+    exponents = letters.map { |letter| CODEX[letter] }
+    exponents.map.with_index { |value, id| value * (BASE**id) }.reduce(:+)
   end
 
   def duplicate?
