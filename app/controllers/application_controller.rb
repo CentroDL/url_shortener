858f4a2106020450b997a43ade24ea7e02e93bfd
Dennis Liaw <dennis.liaw@gmail.com>
2016-08-25 15:38:53 -0400

    adds 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def not_found
-    raise ActionController::RoutingError.new('Not Found')
+    render status: 404
   end
 
 end
